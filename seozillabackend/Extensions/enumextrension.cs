f1295028d94e3dfe6650153e9f6cd026a378e301
prashanttends1 <prashanttends1@gmail.com>
2018-01-31 09:05:44 +0530

    Order_model_change

---
+++ seozillabackend/Extensions/enumextrension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace seozillabackend.Extensions
+{
+    public static class enumextrension
+    {
+        public static string DisplayName(this Enum value)
+        {
+            // the following is my variation on the extension method you linked to
+            if (value == null)
+            {
+                return null;
+            }
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            return value.ToString();
+        }
+    }
+}

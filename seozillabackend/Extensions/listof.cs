0709927ca9f6ae84d256f9c893af81df38a0dd75
prashanttends1 <prashanttends1@gmail.com>
2018-02-28 19:12:40 +0530

    users_edit

---
+++ seozillabackend/Extensions/listof.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace seozillabackend.Extensions
+{
+    public class listof
+    {
+        public static List<SelectListItem> countries = new List<SelectListItem>() {                 new SelectListItem(){ Text="Afghanistan", Value="Afghanistan"},                new SelectListItem(){ Text="India", Value="India"},};
+
+    }
+}

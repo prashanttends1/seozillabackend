8ede791646b9babdd1c48ecef8662bb068e03f51
Sakshi2Tyagi <sakshi2tyagi@gmail.com>
2018-05-25 15:21:06 +0530

    links_hr_name_tooltip_added

--- seozillabackend/Models/zillablog.cs
+++ seozillabackend/Models/zillablog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,17 @@ namespace seozillabackend.Models
     public class zillablog
     {
         public int ID { get; set; }
+        [Display(Name = "Words")]
         public string words { get; set; }
+        [Display (Name="Quantity")]
         public int quantity { get; set; }
+        [Display (Name="Title")]
         public string title { get; set; }
+        [Display (Name="Website")]
         public string website { get; set; } //target website or audience
+        [Display (Name="Brief")]
         public string brief { get; set; } //project brief
+        [Display(Name="Document Url")]
         public string document_url { get; set; }
         public int orderID { get; set; }
         public virtual order order { get; set; }
--- seozillabackend/Models/zillax.cs
+++ seozillabackend/Models/zillax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,14 +10,23 @@ namespace seozillabackend.Models
     {
        
         public int ID { get; set; }
+        [Display(Name = "Plan")]
         public string plan { get; set; }
+        [Display(Name = "Keyword 1")]
         public string keyword1 { get; set; }
+          [Display(Name = "Keyword 2")]
         public string keyword2 { get; set; }
+          [Display(Name = "Keyword 3")]
         public string keyword3 { get; set; }
+          [Display(Name = "Competitor 1")]
         public string competitor1{get; set;}
+         [Display(Name = "Competitor 2")]
         public string competitor2{get; set;}
+         [Display(Name = "Competitor 3")]
         public string competitor3{get; set;}
+         [Display(Name = "Target Location")]
         public string targetlocation{get; set;}
+         [Display(Name = "Other Information")]
         public string otherinfo{get; set;}
         public int orderID { get; set; }
         public virtual order order { get; set; }

92d03320602c2e9e4f35affa3728f6734fad927a
Sakshi2Tyagi <sakshi2tyagi@gmail.com>
2018-06-05 18:17:34 +0530

    error_messages

---
+++ seozillabackend/assets/js/validation.js
@@ -0,0 +1,16 @@
+﻿
+       $(function () {
+           $("#errormessage").css({ "display": "none" });
+           $(".btn-custom").click(function () {
+               var password = $("#txtPassword").val();
+               var confirmPassword = $("#txtConfirmPassword").val();
+               if (password != confirmPassword) {
+                   $("#errormessage").css({ "display": "initial" });
+                   $("#txtPassword").val(null);
+                   $("#txtConfirmPassword").val(null);
+                   return false;
+                
+               }
+               return true;
+           });
+       });
